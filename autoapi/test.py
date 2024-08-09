e7afdf62af9b6f345bdac643de2a0b41c3ad099b
oliver <oliverbcontact@gmail.com>
2024-08-09 08:43:15 -0400

    Delete test.py

--- autoapi/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-# res = requests.put("http://localhost:5000/artists/1", params={'hi': 'world'}, json=json.dumps({"Name": "AC/DCB"}))
-res = requests.post("http://localhost:5000/artists", json={"Name": "Drake"})
-print(res.status_code, res.content)
-print(requests.get("http://localhost:5000/artists").json())
